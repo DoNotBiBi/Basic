92cb368515170a6b9e167f644f84e496582fbe04
wangkang <wk941218@outlook.com>
2020-10-12 10:42:43 +0800

    hello

--- main.py
+++ main.py
@@ -20,15 +20,9 @@ from Time import *
 from Threading import *
 import queue
 from Base import *
-import cv2 as cv
-import numpy as np
 # todo:
 #  1.把git学好，永熟
 #  2.整理简单题目
 if __name__ == '__main__':
-    img = cv.imread('1.png', cv.IMREAD_COLOR)
-    cv.imshow('image', img)
-    cv.waitKey(0)
-    ## 测试注释
-    cv.destroyAllWindows() ##hello
+    print("hello")
 
